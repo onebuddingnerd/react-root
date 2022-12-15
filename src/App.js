6a29e189c63b14eb695025059e5818ce17b112ef
Anupam Pokharel <avikrk88@dhcp-9-240-117-173.bnt-us.ibmmobiledemo.com>
2022-12-15 10:20:35 -0600

    why no online sync

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+import cv_ico from "./imgdata/cv-material.png"
+import home_ico from "./imgdata/home-material.png"
+import github_ico from "./imgdata/github-material.png"
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>Currently in development</h1>
+    
+        <p>This webpage will be up and running soon. In the meantime, please feel free to take a look at my <a href={require("./filesdata/CV_online.pdf")}>CV</a> </p>
+        <p>&mdash; Anupam Pokharel a.k.a. <a href="https://github.com/onebuddingnerd"> onebuddingnerd </a> </p>
+        <br/><br/>
+        <center>
+        <h2>
+          <a href="https://onebuddingnerd.github.io/react-root"><img src={home_ico}/></a> 
+          <a href={require("./filesdata/CV_online.pdf")}> <img src={cv_ico}/></a> 
+          <a href="https://github.com/onebuddingnerd"><img src={github_ico}/></a> 
+        </h2>
+        </center>
     </div>
   );
 }
